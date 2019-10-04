c4071d3614a4cace581755103dc19c4f63275a0a
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2019-10-04 16:42:14 -0300

    Rota para recuperar o arquivo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -14,6 +15,8 @@ mongoose.connect('mongodb+srv://carol:!thiS0411@aircnc-qqzky.mongodb.net/aircnc?
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
 
