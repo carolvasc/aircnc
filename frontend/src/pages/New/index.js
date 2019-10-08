c73051d3593a9b878114547f496c26a95d4f51b6
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2019-10-08 15:59:00 -0300

    Upload e preview da thumbnail

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
 
 export default function New() {
   const [company, setCompany] = useState('');
   const [techs, setTechs] = useState('');
   const [price, setPrice] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null
+  }, [thumbnail]);
 
   function handleSubmit() {
 
@@ -11,6 +20,15 @@ export default function New() {
 
   return (
     <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? 'has-thumbnail' : ''}
+      >
+        <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+        <img src={camera} alt="Select img" />
+      </label>
+
       <label htmlFor="company">EMPRESA *</label>
       <input
         id="company"
