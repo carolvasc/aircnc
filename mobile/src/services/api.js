cc806fe55dac1d4db3447299f0351b19fdfb6a6d
Carol Vasconcelos <cvsouza04@gmail.com>
2020-05-04 22:59:03 -0300

    Finalização do mobile

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // URL do Expo, para funcionar nos dispositivos moveis e nao apenas nos emuladores
-  baseURL: 'http://192.168.0.46:3333',
+  baseURL: 'http://192.168.8.10:3333',
 });
 
 export default api;
